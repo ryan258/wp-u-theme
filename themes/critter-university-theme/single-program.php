47821a3375b7a85072fe57c5148a4077ae0640d5
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-12-10 12:28:51 -0600

    Create a block on Programs CT the lists Events referencing it.

--- themes/critter-university-theme/single-program.php
+++ themes/critter-university-theme/single-program.php
@@ -18,6 +18,56 @@
         <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?></span></p>
       </div>
       <div class="generic-content"><?php the_content(); ?></div>
+      <?php
+      $today = date('Ymd');
+        // define the view
+        $homepageEvents = new WP_Query(array(
+          'posts_per_page' => 3,
+          'post_type' => 'event',
+          'meta_key' => 'event_date',
+          'orderby' => 'meta_value_num',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'event_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            ),
+            array(
+              'key' => 'related_programs',
+              'compare' => 'LIKE', // LIKE ~= contains
+              'value' => '"' . get_the_ID() . '"'// in quotes becaue WO serializes values with quotes
+            )
+          ),
+        ));
+
+        while ($homepageEvents->have_posts()) {
+          // get data ready for each post
+          $homepageEvents->the_post(); ?>
+          
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month"><?php
+                $eventDate = new DateTime(get_field('event_date'));
+                echo $eventDate->format('M');
+              ?></span>
+              <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+              <p><?php
+                if (has_excerpt()) {
+                  echo get_the_excerpt();
+                } else {
+                  echo wp_trim_words(get_the_content(), 18);
+                }
+              ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+
+        <?php }
+      ?>
     </div>
     
   <?php }
