cba96d558b92978b79402ea766d77a6dd73b81bb
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-12-10 11:37:18 -0600

    Fix program archive sorting.

--- themes/critter-university-theme/functions.php
+++ themes/critter-university-theme/functions.php
@@ -31,7 +31,7 @@ add_action('after_setup_theme', 'university_features');
 function university_adjust_queries($query) {
   // alter the query for the program archive
   if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
-    $query->set('orderby', 'meta_value_num');
+    $query->set('orderby', 'title');
     $query->set('order', 'ASC');
     $query->set('posts_per_page', -1);
   }
