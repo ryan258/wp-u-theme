5ee16196a10a8d3807e9ced3132f24097955d466
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-12-14 09:43:39 -0600

    Consolidate event templates into a template-part and reduce duplicate event list template code.

--- themes/critter-university-theme/archive-event.php
+++ themes/critter-university-theme/archive-event.php
@@ -17,21 +17,10 @@ get_header(); ?>
   <?php
     // loop through all posts
     while(have_posts()) {
-      the_post(); ?>
-      <div class="event-summary">
-        <a class="event-summary__date t-center" href="#">
-          <span class="event-summary__month"><?php
-            $eventDate = new DateTime(get_field('event_date'));
-            echo $eventDate->format('M');
-          ?></span>
-          <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-        </a>
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-          <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-        </div>
-      </div>
-    <?php } wp_reset_postdata(); // runs clean for our queries
+      the_post(); 
+      // use the file template-parts/content-event.php
+      get_template_part('template-parts/content', 'event');  
+    } wp_reset_postdata(); // runs clean for our queries
     echo paginate_links();
   ?>
   <hr class="section-break" />
--- themes/critter-university-theme/page-past-events.php
+++ themes/critter-university-theme/page-past-events.php
@@ -34,21 +34,11 @@ get_header(); ?>
     ));
     // loop through all posts
     while($pastEvents->have_posts()) {
-      $pastEvents->the_post(); ?>
-      <div class="event-summary">
-        <a class="event-summary__date t-center" href="#">
-          <span class="event-summary__month"><?php
-            $eventDate = new DateTime(get_field('event_date'));
-            echo $eventDate->format('M');
-          ?></span>
-          <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-        </a>
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-          <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-        </div>
-      </div>
-    <?php } wp_reset_postdata(); // runs clean for our queries
+      $pastEvents->the_post(); 
+      // use the file template-parts/content-event.php
+      get_template_part('template-parts/content', 'event');
+    } wp_reset_postdata(); // runs clean for our queries
+    
     echo paginate_links(array(
       'total' => $pastEvents->max_num_pages
     ));
