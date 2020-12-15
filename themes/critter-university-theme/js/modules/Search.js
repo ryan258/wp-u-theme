6f5dd685846cdc21b33b79f69302264e3ad0e22b
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-12-15 12:51:44 -0600

    Additional UX tweaks.

--- themes/critter-university-theme/js/modules/Search.js
+++ themes/critter-university-theme/js/modules/Search.js
@@ -4,6 +4,7 @@ class Search {
   // 1.) describe objectevery class needs a constructor! 🤗
   constructor() {
     // what executes whenever you instantiate an object
+    this.addSearchHTML()
     this.resultsDiv = $("#search-overlay__results")
     this.openButton = $(".js-search-trigger")
     this.closeButton = $(".search-overlay__close")
@@ -34,7 +35,7 @@ class Search {
           this.resultsDiv.html("<div class='spinner-loader'></div>")
           this.isSpinnerVisible = true
         }
-        this.typingTimer = setTimeout(this.getResults.bind(this), 2000)
+        this.typingTimer = setTimeout(this.getResults.bind(this), 750)
       } else {
         this.resultsDiv.html("")
         this.isSpinnerVisible = false
@@ -71,6 +72,8 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active")
     $("body").addClass("body-no-scroll")
+    this.searchField.val("")
+    setTimeout(() => this.searchField.focus(), 301)
     // console.log("opening...")
     this.isOverlayOpen = true
   }
@@ -80,6 +83,25 @@ class Search {
     $("body").removeClass("body-no-scroll")
     // console.log("closing...")
     this.isOverlayOpen = false
+  }
+
+  addSearchHTML() {
+    $("body").append(`
+      <div class="search-overlay">
+        <div class="search-overlay__top">
+          <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" id="search-term" class="search-term" placeholder="What are you looking for?" autocomplete="off">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="container">
+          <div id="search-overlay__results">
+            holla!
+          </div>
+        </div>
+      </div>
+    `)
   }
 }
 
