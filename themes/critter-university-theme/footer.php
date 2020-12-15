08310e8f2423bab8789e10c55551a3e416cdcd1a
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-12-15 09:10:42 -0600

    Add spinner and make UX search enhancements.

--- themes/critter-university-theme/footer.php
+++ themes/critter-university-theme/footer.php
@@ -67,6 +67,11 @@
       <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
     </div>
   </div>
+  <div class="container">
+    <div id="search-overlay__results">
+      holla!
+    </div>
+  </div>
 </div>
 
 <?php wp_footer(); ?>
--- themes/critter-university-theme/js/modules/Search.js
+++ themes/critter-university-theme/js/modules/Search.js
@@ -4,25 +4,53 @@ class Search {
   // 1.) describe objectevery class needs a constructor! 🤗
   constructor() {
     // what executes whenever you instantiate an object
+    this.resultsDiv = $("#search-overlay__results")
     this.openButton = $(".js-search-trigger")
     this.closeButton = $(".search-overlay__close")
     this.searchOverlay = $(".search-overlay")
+    this.searchField = $("#search-term")
     // call event listeners on load
     this.events()
     // track w/o using the DOM for performance
     this.isOverlayOpen = false
+    this.isSpinnerVisible = false
+    this.previousValue
+    this.typingTimer
   }
   // 2.) describe events - connect the dots
   events() {
     this.openButton.on("click", this.openOverlay.bind(this))
     this.closeButton.on("click", this.closeOverlay.bind(this))
     $(document).on("keydown", this.keyPressDispatcher.bind(this))
+    this.searchField.on("keyup", this.typingLogic.bind(this))
   }
 
   // 3.) methods - our stock of actions
+  typingLogic() {
+    if (this.searchField.val() != this.previousValue) {
+      clearTimeout(this.typingTimer) // reset timer with each keydown
+      if (this.searchField.val()) {
+        if (!this.isSpinnerVisible) {
+          this.resultsDiv.html("<div class='spinner-loader'></div>")
+          this.isSpinnerVisible = true
+        }
+        this.typingTimer = setTimeout(this.getResults.bind(this), 2000)
+      } else {
+        this.resultsDiv.html("")
+        this.isSpinnerVisible = false
+      }
+    }
+    this.previousValue = this.searchField.val()
+  }
+
+  getResults(e) {
+    this.resultsDiv.html("imagine content")
+    this.isSpinnerVisible = false
+  }
+
   keyPressDispatcher(e) {
     // console.log(e.keyCode)
-    if (e.keyCode === 83 && !this.isOverlayOpen) {
+    if (e.keyCode === 83 && !this.isOverlayOpen && !$("input, textarea").is(":focus")) {
       this.openOverlay()
     }
     if (e.keyCode === 27 && this.isOverlayOpen) {
