846813ac441bf1a5622b4b4ec3bc34d4d0596c9b
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-12-18 11:19:01 -0600

    Handle multiple posts types in REST.

--- themes/critter-university-theme/inc/search-route.php
+++ themes/critter-university-theme/inc/search-route.php
@@ -18,8 +18,15 @@ function universityRegisterSearch() {
 function universitySearchResults($data) {
   // WP will automatically convert our data from PHP to JSON
   // so we can just focus on writing custom queries 🤗
-  $professors = new WP_Query(array(
-    'post_type' => 'professor',
+  $mainQuery = new WP_Query(array(
+    'post_type' => array(
+      'post',
+      'page',
+      'professor',
+      'program',
+      'campus',
+      'event'
+    ),
     // 's' stands for search, special query prop, and rock a parameter
     // look in the data array for term, we own the naming scheme
     // wp-json/university/v1/search?term=dr
@@ -28,20 +35,57 @@ function universitySearchResults($data) {
   ));
 
   //define data that we want
-  $professorResults  = array();
+  $results  = array(
+    'generalInfo' => array(),
+    'professors' => array(),
+    'programs' => array(),
+    'events' => array(),
+    'campuses' => array()
+  );
   // loop and push data to our empty array
-  while ($professors->have_posts()) {
+  while ($mainQuery->have_posts()) {
     // get the data ready and accessible
-    $professors->the_post();
+    $mainQuery->the_post();
     // get what we want
     // array_push(arrayToAddTo, whatToAddToThatArray);
-    array_push($professorResults, array(
-      'title' => get_the_title(),
-      'permalink' => get_the_permalink(),
-    ));
+    if (get_post_type() == 'post' OR get_post_type() == 'page') {
+      array_push($results['generalInfo'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+    
+    if (get_post_type() == 'professor') {
+      array_push($results['professors'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+    
+    if (get_post_type() == 'program') {
+      array_push($results['programs'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+    
+    if (get_post_type() == 'event') {
+      array_push($results['events'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+    
+    if (get_post_type() == 'campus') {
+      array_push($results['campuses'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+
   }
 
   // return the data we'd otherwise be looping through
   // return $professors->posts;
-  return $professorResults;
+  return $results;
 }
