32026148dbb796e17356ce2715834c73aa307c4f
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-01-13 15:02:06 -0600

    Set up fallback search for when JS is unavailable.

--- themes/critter-university-theme/js/modules/Search.js
+++ themes/critter-university-theme/js/modules/Search.js
@@ -47,26 +47,26 @@ class Search {
 
   getResults(e) {
     // we'll get the content of all types from our new custom query/end point
-    $.getJSON(universityData.root_url + "/wp-json/university/v1/search?term=" + this.searchField.val(), results => {
+    $.getJSON(universityData.root_url + "/wp-json/university/v1/search?term=" + this.searchField.val(), (results) => {
       this.resultsDiv.html(`
         <div class="row">
           <div class="one-third">
             <h2 class="search-overlay__section-title">General Information</h2>
             ${results.generalInfo.length ? '<ul class="link-list min-list">' : "<p>No general information matches that search.</p>"}
-              ${results.generalInfo.map(item => `<li><a href="${item.permalink}">${item.title}</a>${item.postType == "post" ? ` by ${item.authorName}` : ""}</li>`).join("")}
+              ${results.generalInfo.map((item) => `<li><a href="${item.permalink}">${item.title}</a>${item.postType == "post" ? ` by ${item.authorName}` : ""}</li>`).join("")}
             ${results.generalInfo.length ? "</ul>" : ""}
           </div>
           <div class="one-third">
             <h2 class="search-overlay__section-title">Programs</h2>
             ${results.programs.length ? '<ul class="link-list min-list">' : `<p>No programs match that search. <a href="${universityData.root_url}/programs">View Programs</a> 👻</p>`}
-              ${results.programs.map(item => `<li><a href="${item.permalink}">${item.title}</a></li>`).join("")}
+              ${results.programs.map((item) => `<li><a href="${item.permalink}">${item.title}</a></li>`).join("")}
             ${results.programs.length ? "</ul>" : ""}
             
             <h2 class="search-overlay__section-title">Professors</h2>
             ${results.professors.length ? '<ul class="professor-cards">' : `<p>No professors match that search. </p>`}
               ${results.professors
                 .map(
-                  item => `
+                  (item) => `
                 <li class="professor-card__list-item">
                   <a class="professor-card" href="${item.permalink}">
                     <img src="${item.image}" alt="<?php the_title(); ?>" class="professor-card__image">
@@ -81,14 +81,14 @@ class Search {
           <div class="one-third">
             <h2 class="search-overlay__section-title">Campuses</h2>
             ${results.campuses.length ? '<ul class="link-list min-list">' : `<p>No campuses match that search. <a href="${universityData.root_url}/campuses">View Campuses</a></p>`}
-              ${results.campuses.map(item => `<li><a href="${item.permalink}">${item.title}</a></li>`).join("")}
+              ${results.campuses.map((item) => `<li><a href="${item.permalink}">${item.title}</a></li>`).join("")}
             ${results.campuses.length ? "</ul>" : ""}
             
             <h2 class="search-overlay__section-title">Events</h2>
             ${results.events.length ? "" : `<p>No events match that search. <a href="${universityData.root_url}/events">View All Events</a></p>`}
             ${results.events
               .map(
-                item => `
+                (item) => `
                   <div class="event-summary">
                     <a class="event-summary__date t-center" href="${item.permalink}">
                       <span class="event-summary__month">${item.month}</span>
@@ -127,6 +127,8 @@ class Search {
     setTimeout(() => this.searchField.focus(), 301)
     // console.log("opening...")
     this.isOverlayOpen = true
+    // prevent default behavior of link elements
+    return false
   }
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active")
---
+++ themes/critter-university-theme/page-search.php
@@ -0,0 +1,67 @@
+<?php
+  get_header();
+  while(have_posts()) {
+    the_post(); 
+    pageBanner(array(
+      // 'title' => 'I am the titliest title!',
+      // 'subtitle' => 'Hi, this is the subtitle.',
+      // 'photo' => 'https://image.shutterstock.com/z/stock-vector-rick-and-morty-cartoon-portal-gun-background-abstract-green-and-yellow-colors-vortex-vector-1552796555.jpg'
+    ));
+?>    
+
+    <div class="container container--narrow page-section">
+
+      
+      
+      <?php 
+        $theParentID = wp_get_post_parent_id(get_the_ID());
+        if ($theParentID) { ?>
+          <div class="metabox metabox--position-up metabox--with-home-link">
+            <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentID); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParentID); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+          </div>
+        <?php }
+      ?>
+
+      <?php 
+      $testArray = get_pages(array(
+        'child_of' => get_the_ID()
+      ));
+
+      if ($theParentID or $testArray) { ?>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParentID); ?>"><?php echo get_the_title($theParentID); ?></a></h2>
+        <ul class="min-list">
+          <?php
+            if ($theParentID) {
+              $findChildrenOf = $theParentID;
+            } else {
+              $findChildrenOf = get_the_ID();
+            }
+            wp_list_pages(array(
+              'title_li' => NULL,
+              'child_of' => $findChildrenOf,
+              'sort_column' => 'menu_order'
+            ));
+          ?>
+        </ul>
+      </div>
+      <?php } ?>
+
+      <div class="generic-content">
+        <!-- Get the main content -->
+        <!-- esc_url() is a best practice -->
+        <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
+            <label class="headline headline--medium" for="s">Perform a New Search</label>
+            <div class="search-form-row">
+              <input class="s" id="s" type="search" name="s" placeholder="What are you looking for?">
+              <input class="search-submit" type="submit" value="Search">
+            </div>
+        </form>
+      </div>
+
+    </div>
+    
+  <?php }
+  get_footer();
+?>
+
