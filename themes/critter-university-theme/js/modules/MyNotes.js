06502845413edc67f8eb47747c7dda2ad182b9a4
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-01-19 09:30:36 -0600

    Enable toggling on notes between edit/read states.

--- themes/critter-university-theme/js/modules/MyNotes.js
+++ themes/critter-university-theme/js/modules/MyNotes.js
@@ -9,9 +9,36 @@ class MyNotes {
 
   events() {
     $(".delete-note").on("click", this.deleteNote)
+    // make sure we bind there to the class and not the thing that was clicked
+    $(".edit-note").on("click", this.editNote.bind(this))
   }
 
   // Methods will go here
+  editNote(e) {
+    const thisNote = $(e.target).parents("li")
+    if (thisNote.data("state") == "editable") {
+      // make read only
+      this.makeNoteReadOnly(thisNote)
+    } else {
+      // make editable
+      this.makeNoteEditable(thisNote)
+    }
+  }
+
+  makeNoteEditable(thisNote) {
+    thisNote.find(".edit-note").html('<i class="fa fa-times" aria-hidden="true"></i> Cancel')
+    thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field")
+    thisNote.find(".update-note").addClass("update-note--visible")
+    thisNote.data("state", "editable")
+  }
+
+  makeNoteReadOnly(thisNote) {
+    thisNote.find(".edit-note").html('<i class="fa fa-pencil" aria-hidden="true"></i> Edit')
+    thisNote.find(".note-title-field, .note-body-field").attr("readonly", "readonly").removeClass("note-active-field")
+    thisNote.find(".update-note").removeClass("update-note--visible")
+    thisNote.data("state", "cancel")
+  }
+
   deleteNote(e) {
     // alert("ok deleted")
     const thisNote = $(e.target).parents("li")
